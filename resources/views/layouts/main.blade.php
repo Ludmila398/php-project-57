30240b6c240caeaf47227713947e58259efe559b
Liudmila <liudmilagubanova18@gmail.com>
2023-11-11 20:27:27 +0400

    add changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
                     
                     <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <!--проверила что выше -->
+                           
                          
                           <!-- сделать позже задания статусы и метки-->
                             <li>
@@ -54,16 +54,11 @@
 <!--проверила что выше -->
         <section class="bg-white dark:bg-gray-900">
             <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-                                    <div class="mr-auto place-self-center lg:col-span-7">
-        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-            Привет от Хекслета!        </h1>
-        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            Это простой менеджер задач на Laravel        </p>
-        <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-            <a href="https://ashikov.ru" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" target="_blank">
-                Нажми меня            </a>
-        </div>
-    </div>
+
+            
+            
+
+            @yield('content')
             </div>
         </section>
     </div>
