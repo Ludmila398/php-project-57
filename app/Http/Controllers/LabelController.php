902d5676b46ace57eeb81cf0bf8b6af832332bf4
Liudmila <liudmilagubanova18@gmail.com>
2023-12-10 21:23:04 +0400

    add changes

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Label;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreLabelRequest;
+use App\Http\Requests\UpdateLabelRequest;
 
 class LabelController extends Controller
 {
@@ -21,15 +24,29 @@ class LabelController extends Controller
      */
     public function create()
     {
-        //
+        if (Auth::user() === null) {
+            abort(403);
+        }
+
+        $label = new Label();
+        return view('Label.create', compact('label'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreLabelRequest $request)
     {
-        //
+        if (Auth::user() === null) {
+            abort(403);
+        }
+        $data = $request->validated();
+
+        Label::create($data);
+
+        flash(__('messages.label.created'))->success();
+
+        return redirect()->route('labels.index');
     }
 
     /**
@@ -37,15 +54,28 @@ class LabelController extends Controller
      */
     public function edit(Label $label)
     {
-        //
+        if (Auth::user() === null) {
+            abort(403);
+        }
+        return view('Label.edit', compact('label'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Label $label)
+    public function update(UpdateLabelRequest $request, Label $label)
     {
-        //
+        if (Auth::user() === null) {
+            abort(403);
+        }
+
+        $data = $request->validated();
+
+        $label->update($data);
+
+        flash(__('messages.label.modified'))->success();
+
+        return redirect()->route('labels.index');
     }
 
     /**
@@ -53,6 +83,18 @@ class LabelController extends Controller
      */
     public function destroy(Label $label)
     {
-        //
+        if (Auth::user() === null) {
+            abort(403);
+        }
+
+        if ($label->tasks()->exists()) {
+            flash(__('messages.label.deleted.error'))->error();
+        } else {
+            $label->delete();
+
+            flash(__('messages.label.deleted'))->success();
+        };
+
+        return redirect()->route('labels.index');
     }
 }
