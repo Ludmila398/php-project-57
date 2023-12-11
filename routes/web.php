c77e2276254ff3ffcd1578e9172ac48b10e7de7e
Liudmila <liudmilagubanova18@gmail.com>
2023-12-11 21:57:31 +0400

    add changes

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 })->name('home');
 
 ///пока не удалять эти строки, иначе тесты не проходят! удалить вместе с тестами на редакт и удалене профиля!
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 ///пока не удалять эти строки, иначе тесты не проходят! удалить вместе с тестами на редакт и удалене профиля!
 
 require __DIR__ . '/auth.php';
