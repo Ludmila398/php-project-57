8fc41c8724ecfdd80e847e48430f3bd085907c3b
Liudmila <liudmilagubanova18@gmail.com>
2023-12-14 19:56:02 +0400

    add changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,9 +47,9 @@
                 {{ __('Уже зарегистрированы?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
                 {{ __('Зарегистрировать') }}
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
